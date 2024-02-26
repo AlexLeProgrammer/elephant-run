31ab09c0d280861c8652587235d92c67149d5fb6
AlexLeProgrammer <alex.etienne@divtec.ch>
2024-02-26 09:07:59 +0100

    wall + end-screen.

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const CTX = CANVAS.getContext("2d");
 const SCORE_TEXT = document.querySelector("#score");
 
 // Gravity
-const GRAVITY_FORCE = 0.1;
+const GRAVITY_FORCE = 0.2;
 
 // Road
 const ROAD_WIDTH = 1000;
@@ -27,10 +27,11 @@ const PLAYER_WIDTH = 100;
 const PLAYER_HEIGHT = 100;
 const PLAYER_DEPTH = 100;
 
-const PLAYER_DEFAULT_SPEED = 1;
-const PLAYER_ACCELERATION = 0.0001;
+const PLAYER_DEFAULT_SPEED = 2;
+const PLAYER_MAX_SPEED = 20;
+const PLAYER_ACCELERATION = 0.0005;
 const PLAYER_HORIZONTAL_SPEED = 6;
-const PLAYER_JUMP_FORCE = 10;
+const PLAYER_JUMP_FORCE = 9;
 
 const DEFAULT_PLAYER_LOCATION = {x: -200, y: 118, z: PLAYER_DEPTH / 2};
 
@@ -49,9 +50,10 @@ const LITTLE_WALL_MAX_HEIGHT = PLAYER_HEIGHT;
 // Player
 let playerLocation = {x: 0, y: DEFAULT_PLAYER_LOCATION.y, z: 0};
 let playerYVelocity = 0;
-let playerColumn = 0;
 let playerGoalColumn = 0;
 let playerSpeed = PLAYER_DEFAULT_SPEED;
+
+let score = 0;
 
 // Inputs
 let leftPressed = false;
@@ -67,7 +69,8 @@ let walls = [
     [],[],[]
 ];
 
-let lastWallsStart = null;
+let lastWallsTime = null;
+let lastMaxWallWidth = 1;
 
 //#endregion
 
@@ -89,10 +92,12 @@ function degToRad(degrees) {
  * @param width Width of the box.
  * @param height Height of the box.
  * @param depth Depth of the box.
+ * @param fillColor Color of the faces of the box.
+ * @param strokeColor Color of the edges of the box.
  */
-function strokeBox(x, y, z, width, height, depth) {
-    CTX.strokeStyle = "black";
-    CTX.fillStyle = "white";
+function strokeBox(x, y, z, width, height, depth, fillColor = "white", strokeColor = "black") {
+    CTX.strokeStyle = strokeColor;
+    CTX.fillStyle = fillColor;
     let startPoint = [CANVAS.width / 2 + x - Math.cos(degToRad(45)) * z,
         CANVAS.height / 2 + y - Math.sin(degToRad(45)) * z];
 
@@ -125,16 +130,28 @@ function strokeBox(x, y, z, width, height, depth) {
  * Create walls of a "wave".
  */
 function createWalls() {
-    let height = Math.floor(Math.random() * WALL_MAX_HEIGHT);
-
     let columnFull = [false, false, false];
+    let isALittleWall = false;
+
+    lastMaxWallWidth = 0;
     for (let i = 0; i < 3; i++) {
         if (Math.floor(Math.random() * 2)) {
-            columnFull[i] = height > LITTLE_WALL_MAX_HEIGHT;
+            let height = Math.floor(Math.random() * WALL_MAX_HEIGHT);
+            if (height > LITTLE_WALL_MAX_HEIGHT) {
+                columnFull[i] = true;
+            } else {
+                isALittleWall = true;
+            }
+
+            let width = Math.floor(Math.random() * WALL_MAX_WIDTH);
+            if (width > lastMaxWallWidth) {
+                lastMaxWallWidth = width;
+            }
+
             walls[i].push({
                 x: ROAD_WIDTH,
                 y: DEFAULT_PLAYER_LOCATION.y + PLAYER_HEIGHT - height,
-                width: Math.floor(Math.random() * WALL_MAX_WIDTH),
+                width: width,
                 height: height
             });
         }
@@ -143,9 +160,11 @@ function createWalls() {
     if (columnFull[0] && columnFull[1] && columnFull[2]) {
         let index = Math.floor(Math.random() * 3);
         walls[index].splice(walls[index].length - 1, 1);
-    } else if (!columnFull[0] && !columnFull[1] && !columnFull[2]) {
+    } else if (!columnFull[0] && !columnFull[1] && !columnFull[2] && !isALittleWall) {
         createWalls();
     }
+
+
 }
 
 /**
@@ -153,12 +172,17 @@ function createWalls() {
  */
 function getGroundDistance() {
     let minDist = null;
-    for (let wall of walls[playerColumn]) {
-        let dist = wall.y - playerLocation.y - PLAYER_HEIGHT
-        if (wall.y >= playerLocation.y + PLAYER_HEIGHT &&
-            DEFAULT_PLAYER_LOCATION.x + PLAYER_WIDTH > wall.x && DEFAULT_PLAYER_LOCATION.x < wall.x + wall.width &&
-            (minDist === null || (dist >= 0 && dist < minDist))) {
-            minDist = dist;
+    for (let i = 0; i < walls.length; i++) {
+        for (let wall of walls[i]) {
+            let z = i < 2 ? i === 0 ? 0 : ROAD_DEPTH / 6 : ROAD_DEPTH / 6 * 2;
+            let dist = wall.y - playerLocation.y - PLAYER_HEIGHT
+            if (wall.y >= playerLocation.y + PLAYER_HEIGHT &&
+                DEFAULT_PLAYER_LOCATION.x + PLAYER_WIDTH > wall.x && DEFAULT_PLAYER_LOCATION.x < wall.x + wall.width &&
+                DEFAULT_PLAYER_LOCATION.z + playerLocation.z + PLAYER_DEPTH / 2 > z &&
+                DEFAULT_PLAYER_LOCATION.z + playerLocation.z < z + ROAD_DEPTH / 6 &&
+                (minDist === null || (dist >= 0 && dist < minDist))) {
+                minDist = dist;
+            }
         }
     }
 
@@ -178,9 +202,14 @@ function tick() {
     lastTick = performance.now();
 
     // Create new wall
-    if (Math.floor(playerLocation.x) !== lastWallsStart && Math.floor(playerLocation.x) % (WALL_MAX_WIDTH * 2) === 0) {
+    if (Math.floor(performance.now() / 10) !== lastWallsTime &&
+        (
+            (playerSpeed < 10 && Math.floor(performance.now() / 10) % 500 === 0) ||
+            (playerSpeed >= 10 && Math.floor(performance.now() / 10) % 300 === 0)
+
+        )) {
         createWalls();
-        lastWallsStart = Math.floor(playerLocation.x);
+        lastWallsTime = Math.floor(performance.now() / 10);
     }
 
     // Move
@@ -194,14 +223,16 @@ function tick() {
     playerLocation.x += playerSpeed * deltaTime;
 
     // Accelerate
-    playerSpeed += PLAYER_ACCELERATION;
+    if (playerSpeed < PLAYER_MAX_SPEED) {
+        playerSpeed += PLAYER_ACCELERATION;
+    }
 
     // Search if the player want to move in another column
-    if (leftPressed && playerGoalColumn < 2 && playerColumn >= playerGoalColumn) {
+    if (leftPressed && playerGoalColumn < 2) {
         playerGoalColumn++;
     }
 
-    if (rightPressed && playerGoalColumn > 0 && playerColumn <= playerGoalColumn) {
+    if (rightPressed && playerGoalColumn > 0) {
         playerGoalColumn--;
     }
 
@@ -213,11 +244,11 @@ function tick() {
         } else {
             playerYVelocity = 0;
         }
-    } else if ((playerYVelocity + GRAVITY_FORCE) * deltaTime > groundDistance) {
+    } else if (playerYVelocity * deltaTime + GRAVITY_FORCE * deltaTime > groundDistance) {
         playerLocation.y += groundDistance;
         playerYVelocity = 0;
     } else {
-        playerYVelocity += GRAVITY_FORCE;
+        playerYVelocity += GRAVITY_FORCE * deltaTime;
     }
 
     playerLocation.y += playerYVelocity * deltaTime;
@@ -234,17 +265,27 @@ function tick() {
         } else {
             playerLocation.z -= PLAYER_HORIZONTAL_SPEED * deltaTime;
         }
-    } else {
-        playerColumn = playerGoalColumn;
     }
 
     // Check if the player is in a wall
-    for (let wall of walls[playerColumn]) {
-        if (DEFAULT_PLAYER_LOCATION.x + PLAYER_WIDTH > wall.x &&
-            DEFAULT_PLAYER_LOCATION.x < wall.x + wall.width &&
-            playerLocation.y + PLAYER_HEIGHT > wall.y && playerLocation.y < wall.y + wall.height) {
-            // Stop the game
-            clearInterval(loop);
+    for (let i = 0; i < walls.length; i++) {
+        for (let wall of walls[i]) {
+            let z = i < 2 ? i === 0 ? 0 : ROAD_DEPTH / 6 : ROAD_DEPTH / 6 * 2;
+            if (DEFAULT_PLAYER_LOCATION.x + PLAYER_WIDTH > wall.x &&
+                DEFAULT_PLAYER_LOCATION.x < wall.x + wall.width &&
+                playerLocation.y + PLAYER_HEIGHT > wall.y && playerLocation.y < wall.y + wall.height &&
+                playerLocation.z + PLAYER_DEPTH / 2 > z && playerLocation.z < z + ROAD_DEPTH / 6) {
+                // Stop the game
+                clearInterval(loop);
+
+                // Set the canvas and the score invisible
+                CANVAS.style.display = "none";
+                SCORE_TEXT.style.display = "none";
+
+                // Fill the end screen
+                document.querySelector("#endScreen").style.display = "block";
+                document.querySelector("#endScore").innerHTML += SCORE_TEXT.innerHTML;
+            }
         }
     }
 
@@ -265,7 +306,7 @@ function tick() {
     CANVAS.height = window.innerHeight;
 
     // Display the score
-    let score = Math.floor(playerLocation.x / 20);
+    score = Math.floor(playerLocation.x / 100);
     let scoreString = "";
     if (score < 10000) {
         scoreString += "0";
@@ -283,22 +324,24 @@ function tick() {
     SCORE_TEXT.innerHTML = scoreString + score;
 
     // Draw the road
-    strokeBox(ROAD_LOCATION.x, ROAD_LOCATION.y, ROAD_LOCATION.z, ROAD_WIDTH, CANVAS.height, ROAD_DEPTH);
+    strokeBox(ROAD_LOCATION.x, ROAD_LOCATION.y, ROAD_LOCATION.z, ROAD_WIDTH, CANVAS.height, ROAD_DEPTH, "skyblue");
 
     // Draw walls behind the player
     for (let wall of walls[2]) {
-        strokeBox(wall.x, wall.y, ROAD_DEPTH / 6 * 2, wall.width, wall.height, ROAD_DEPTH / 3);
-    }
-
-    if (playerColumn < 2) {
-        for (let wall of walls[1]) {
-            strokeBox(wall.x, wall.y, ROAD_DEPTH / 6, wall.width, wall.height, ROAD_DEPTH / 3);
+        if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x >= 0) {
+            strokeBox(wall.x, wall.y, ROAD_DEPTH / 3, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
         }
     }
 
-    if (playerColumn === 0) {
+    if (Math.floor(playerLocation.z + PLAYER_DEPTH / 2) < Math.floor(ROAD_DEPTH / 3)) {
+        for (let wall of walls[1]) {
+            strokeBox(wall.x, wall.y, ROAD_DEPTH / 6, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
+        }
+    }
+
+    if (Math.floor(playerLocation.z + PLAYER_DEPTH / 2) < Math.floor(ROAD_DEPTH / 6)) {
         for (let wall of walls[0]) {
-            strokeBox(wall.x, wall.y, 0, wall.width, wall.height, ROAD_DEPTH / 3);
+            strokeBox(wall.x, wall.y, 0, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
         }
     }
 
@@ -309,19 +352,19 @@ function tick() {
     // Draw the walls in front of the player
     for (let wall of walls[2]) {
         if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x < 0) {
-            strokeBox(wall.x, wall.y, ROAD_DEPTH / 6 * 2, wall.width, wall.height, ROAD_DEPTH / 3);
+            strokeBox(wall.x, wall.y, ROAD_DEPTH / 3, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
         }
     }
 
     for (let wall of walls[1]) {
-        if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x < 0 || playerColumn === 2) {
-            strokeBox(wall.x, wall.y, ROAD_DEPTH / 6, wall.width, wall.height, ROAD_DEPTH / 3);
+        if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x < 0 || Math.floor(playerLocation.z + PLAYER_DEPTH / 2) >= Math.floor(ROAD_DEPTH / 3)) {
+            strokeBox(wall.x, wall.y, ROAD_DEPTH / 6, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
         }
     }
 
     for (let wall of walls[0]) {
-        if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x < 0 || playerColumn > 0) {
-            strokeBox(wall.x, wall.y, 0, wall.width, wall.height, ROAD_DEPTH / 3);
+        if (wall.x + wall.width - DEFAULT_PLAYER_LOCATION.x < 0 || Math.floor(playerLocation.z + PLAYER_DEPTH / 2) >= Math.floor(ROAD_DEPTH / 6)) {
+            strokeBox(wall.x, wall.y, 0, wall.width, wall.height, ROAD_DEPTH / 3, "orange");
         }
     }
 
